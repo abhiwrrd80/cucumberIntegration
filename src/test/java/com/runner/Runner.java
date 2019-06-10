4c2fd1856e1fc10c9dc8fd550ddd1d8baa68074f
abhiwrrd80 <51590858+abhiwrrd80@users.noreply.github.com>
2019-06-10 16:14:15 +0530

    Add files via upload

---
+++ src/test/java/com/runner/Runner.java
@@ -0,0 +1,15 @@
+package com.runner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="Features/exercise2.feature", glue="cucumberProject" , plugin="json:C:\\Users\\training_c2a.05.01\\Desktop\\JAVA\\Day 3\\cucumberProject\\jsonreports.json"
+                                                )
+public class Runner {
+
+	
+	
+}
